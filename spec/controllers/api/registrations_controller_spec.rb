df79566afd90e76dd7aa43d31b9b0d44f0283148
alexisHetic <alex.delvaque@gmail.com>
2016-01-18 18:01:42 +0100

    Add specs to RegistrationsController#create

--- spec/controllers/api/registrations_controller_spec.rb
+++ spec/controllers/api/registrations_controller_spec.rb
@@ -20,9 +20,28 @@ RSpec.describe Api::RegistrationsController, type: :controller do
         post :create, format: :json, :user => { username: "DonutLover", email: "homer@contact.com", password: "KillBart", password_confirmation: "nothing" }
       end
 
-      it "Return 200 and Object with errors" do
+      it "Return a response with a status 401 and an object with errors" do
         expect(response.status).to eq(200)
+        expect(JSON(response.body)['status']).to eq(401)
         expect(JSON(response.body)['errors'].blank?).to eq(false)
+      end
+    end
+
+    context "When all user data are setted" do
+      before do
+        post :create, format: :json, :user => { username: "DonutLover", email: "homer@contact.com", password: "KillBart", password_confirmation: "KillBart" }
+      end
+
+      it "Return a response with a status 201 and the user data" do
+        response_data = JSON(response.body)
+        user = response_data['data']['user']
+
+        expect(response.status).to eq(200)
+        expect(response_data['status']).to eq(201)
+
+        expect(user.blank?).to eq(false)
+        expect(user['username'].blank?).to eq(false)
+        expect(user['email'].blank?).to eq(false)
       end
     end
   end
