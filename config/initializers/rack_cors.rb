ca3523e54fcdb113d84b605216b279734c8973aa
alexisHetic <alex.delvaque@gmail.com>
2016-01-18 17:06:17 +0100

    Update Rack CORS config to authorize all methods

--- config/initializers/rack_cors.rb
+++ config/initializers/rack_cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, "Rack::Cors" do
   allow do
     origins '*'
-    resource '*', :headers => :any, :method => [:get, :post, :put, :patch, :delete, :options]
+    resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options]
   end
 end
