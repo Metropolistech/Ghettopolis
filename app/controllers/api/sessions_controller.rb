26a8d6a07d19432be606c4c9fa2fdac93b51f9de
alexisHetic <alex.delvaque@gmail.com>
2016-01-19 12:39:08 +0100

    Refacto SessionsController#create to handle credentials connection or token connection

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -3,11 +3,22 @@ class Api::SessionsController < Api::BaseController
   before_filter :ensure_params_exist
 
   def create
+    if token_params
+      return open_session
+    else
+      return create_session
+    end
+  end
+
+  def create_session
     @user = User.find_for_database_authentication(email: user_params[:email])
     return invalid_user unless @user
     return invalid_login_attempt unless @user.valid_password?(user_params[:password])
+    @auth_token = JsonWebToken.encode(JsonWebToken.create_user_payload(@user))
+  end
 
-    @auth_token = JsonWebToken.encode("user_email" => @user.email)
+  def open_session
+    authenticate_user_from_jwt!
   end
 
   private
@@ -20,12 +31,22 @@ class Api::SessionsController < Api::BaseController
     end
   end
 
-  def ensure_params_exist
-    return render json: { errors: { message: "Missing parameters" } }, status: 404 if user_params.blank? || user_params[:email].blank? || user_params[:password].blank?
+  def token_params
+    begin
+      params.require(:token)
+    rescue
+      nil
+    end
+  end
 
-    # if user_params[:email].blank? || user_params[:password].blank?
-    #   return render_unauthorized errors: { unauthenticated: ["Incomplete credentials"]}
-    # end
+  def ensure_user_params_exist
+    return user_params.blank? || user_params[:email].blank? || user_params[:password].blank?
+  end
+
+  def ensure_params_exist
+    if ensure_user_params_exist && token_params.blank?
+      return render json: { errors: { message: "Missing parameters" } }, status: 404
+    end
   end
 
   def invalid_login_attempt
