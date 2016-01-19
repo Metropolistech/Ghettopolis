02706c8262e47dce5dfc7be40cee9b413bff8910
alexisHetic <alex.delvaque@gmail.com>
2016-01-19 17:05:36 +0100

    Refacto SessionsController spec and Add token test

--- spec/controllers/api/sessions_controller_spec.rb
+++ spec/controllers/api/sessions_controller_spec.rb
@@ -3,46 +3,52 @@ RSpec.describe Api::SessionsController, type: :controller do
 
   let(:json) { JSON.parse(response.body) }
 
+  def create_user(&block)
+    @user = User.create(username: "DonutLover", email: "homer@contact.com", password: "KillBart", password_confirmation: "KillBart")
+    yield
+  end
+
   describe "POST /api/session" do
-    context "When there is no user data" do
+
+    context "when no request params" do
       before do
         post :create, format: :json
       end
 
-      it "Return a 404" do
+      it "return 404" do
         expect(response.status).to eq(404)
         expect(JSON(response.body)['errors'].blank?).to eq(false)
       end
     end
 
-    context "When there is no email" do
+    context "when no user email" do
       before do
         post :create, format: :json, :user => { password: "KillBart" }
       end
 
-      it "Return a 404" do
+      it "return 404" do
         expect(response.status).to eq(404)
         expect(JSON(response.body)['errors'].blank?).to eq(false)
       end
     end
 
-    context "When there is no password" do
+    context "when no user password" do
       before do
         post :create, format: :json, :user => { email: "homer@contact.com" }
       end
 
-      it "Return a 404" do
+      it "return 404" do
         expect(response.status).to eq(404)
         expect(JSON(response.body)['errors'].blank?).to eq(false)
       end
     end
 
-    context "When user does not exist" do
+    context "when user does not exist" do
       before do
         post :create, format: :json, :user => { email: "homer@contact.com", password: "KillBart" }
       end
 
-      it "Return a response data with a status 401 and an error object" do
+      it "return data status 401 and error object" do
         response_data = JSON(response.body)
 
         expect(response.status).to eq(401)
@@ -51,13 +57,14 @@ RSpec.describe Api::SessionsController, type: :controller do
       end
     end
 
-    context "When the password is incorrect" do
+    context "when user password is incorrect" do
       before do
-        user = User.create(username: "DonutLover", email: "homer@contact.com", password: "KillBart", password_confirmation: "KillBart")
-        post :create, format: :json, :user => { email: "homer@contact.com", password: "KillBartRightNow" }
+        create_user do
+          post :create, format: :json, :user => { email: "homer@contact.com", password: "KillBartRightNow" }
+        end
       end
 
-      it "Return a response data with a status 401 and an error object" do
+      it "return data status 401 and error object" do
         response_data = JSON(response.body)
 
         expect(response.status).to eq(401)
@@ -66,25 +73,52 @@ RSpec.describe Api::SessionsController, type: :controller do
       end
     end
 
-    context "When user exist and the password is correct" do
+    context "When user and password are good" do
       user = nil
 
       before do
-        user = User.create(username: "DonutLover", email: "homer@contact.com", password: "KillBart", password_confirmation: "KillBart")
-        post :create, format: :json, :user => { email: user.email, password: user.password }
+        create_user do
+          post :create, format: :json, :user => { email: @user.email, password: @user.password }
+        end
       end
 
-      it "Return a response data with a status 401 and an error object" do
+      it "return data status 201 and user object with new token" do
         response_data = JSON(response.body)
         returned_user = response_data['data']['user']
 
         expect(response.status).to eq(200)
         expect(response_data['status']).to eq(201)
 
-        expect(returned_user['username']).to eq(user['username'])
-        expect(returned_user['email']).to eq(user['email'])
+        expect(returned_user['username']).to eq(@user['username'])
+        expect(returned_user['email']).to eq(@user['email'])
 
         expect(response_data['data']['token'].blank?).to eq(false)
+      end
+    end
+
+    context "when open session with available token" do
+      user = nil
+      token = nil
+
+      before do
+        create_user do
+          post :create, format: :json, :user => { email: @user.email, password: @user.password }
+          token = JSON(response.body)['data']['token']
+          post :create, format: :json, :token => token.to_s
+        end
+      end
+
+      it "return data status 201 and user object with same token" do
+        response_data = JSON(response.body)
+        returned_user = response_data['data']['user']
+
+        expect(response.status).to eq(200)
+        expect(response_data['status']).to eq(201)
+
+        expect(returned_user['username']).to eq(@user['username'])
+        expect(returned_user['email']).to eq(@user['email'])
+
+        expect(response_data['data']['token']).to eq(token)
       end
     end
   end
