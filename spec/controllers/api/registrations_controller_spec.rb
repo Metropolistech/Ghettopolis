55ead9aa58b39e3f0a7cfb80f0bb3f3e8b6211a1
alexisHetic <alex.delvaque@gmail.com>
2016-01-19 17:26:28 +0100

    Reword RegistrationsController specs

--- spec/controllers/api/registrations_controller_spec.rb
+++ spec/controllers/api/registrations_controller_spec.rb
@@ -4,35 +4,36 @@ RSpec.describe Api::RegistrationsController, type: :controller do
   let(:json) { JSON.parse(response.body) }
 
   describe "POST /api/register" do
-    context "When there is no user params" do
+
+    context "when no user params" do
       before do
         post :create, format: :json, :data => nil
       end
 
-      it "Return a 404" do
+      it "return 404" do
         expect(response.status).to eq(404)
         expect(JSON(response.body)['errors'].blank?).to eq(false)
       end
     end
 
-    context "When user params set wrong passwords" do
+    context "when wrong password_confirmation" do
       before do
         post :create, format: :json, :user => { username: "DonutLover", email: "homer@contact.com", password: "KillBart", password_confirmation: "nothing" }
       end
 
-      it "Return a response with a status 401 and an object with errors" do
+      it "return response status 401 and errors object" do
         expect(response.status).to eq(200)
         expect(JSON(response.body)['status']).to eq(401)
         expect(JSON(response.body)['errors'].blank?).to eq(false)
       end
     end
 
-    context "When all user data are setted" do
+    context "when user data are setted" do
       before do
         post :create, format: :json, :user => { username: "DonutLover", email: "homer@contact.com", password: "KillBart", password_confirmation: "KillBart" }
       end
 
-      it "Return a response with a status 201 and the user data" do
+      it "return response status 201 and user data" do
         response_data = JSON(response.body)
         user = response_data['data']['user']
 
