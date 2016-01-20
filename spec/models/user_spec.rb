36ebeea0c4230b5961ff30591588babf3d09f9fc
alexisHetic <alex.delvaque@gmail.com>
2016-01-20 20:45:54 +0100

    Add User validation specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,91 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validation" do
+    user = create_user(save: false);
+
+    describe "requires a valid email." do
+      context "If email is invalid" do
+        it "cannot validate user" do
+          user.email = "homer"
+          expect(user.valid?).to eq(false)
+          user.email = "homer@lol"
+          expect(user.valid?).to eq(false)
+          user.email = "homer@.fr"
+          expect(user.valid?).to eq(false)
+        end
+      end
+
+      context "If email is valid" do
+        it "can validate user" do
+          user.email = "me@contact.com"
+          expect(user.valid?).to eq(true)
+        end
+      end
+    end
+
+    describe "requires a valid password_confirmation." do
+      context "If password & confirmation are the same" do
+        it "cannot validate user" do
+          user.password_confirmation = "azerty"
+          expect(user.valid?).to eq(false)
+        end
+      end
+
+      context "If password & confirmation are not the same" do
+        it "can validate user" do
+          user.password = "KillBart"
+          user.password_confirmation = "KillBart"
+          expect(user.valid?).to eq(true)
+        end
+      end
+    end
+
+    describe "requires unique username." do
+      context "If record have an already taken username" do
+        it "cannot validate user" do
+          user.email = "another@email.com"
+          expect(user.valid?).to eq(true)
+
+          user_saved = create_user
+
+          expect(user.username).to eq(user_saved.username)
+          expect(user.valid?).to eq(false)
+        end
+      end
+
+      context "If record have an unique username" do
+        it "can validate user" do
+          user_saved = create_user
+          user.username = "anotherUsername"
+          expect(user.username).not_to eq(user_saved.username)
+          expect(user.valid?).to eq(true)
+        end
+      end
+    end
+
+    describe "requires unique email" do
+      context "If record have an already taken email" do
+        it "cannot save user" do
+          user.email = "homer@contact.com"
+          expect(user.valid?).to eq(true)
+
+          user_saved = create_user
+
+          expect(user.email).to eq(user_saved.email)
+          expect(user.valid?).to eq(false)
+        end
+      end
+
+      context "If record have an unique email" do
+        it "can save user" do
+          user_saved = create_user
+          user.email = "another@email.com"
+          expect(user.email).not_to eq(user_saved.email)
+          expect(user.valid?).to eq(true)
+        end
+      end
+    end
+  end
 end
