b47a15362c13663a61e0e934ac72687bead6ea99
alexisHetic <alex.delvaque@gmail.com>
2016-01-21 23:48:19 +0100

    Add Me::UserController#create and route post /me/projects

--- app/controllers/api/v1/me/projects_controller.rb
+++ app/controllers/api/v1/me/projects_controller.rb
@@ -4,7 +4,11 @@ class Api::V1::Me::ProjectsController < Api::BaseController
   end
 
   def create
-
+    if projects_params
+      @project = current_user.create_project!(data: projects_params)
+      return @project ? @project : render_error(message: "Cannot create project")
+    end
+    render_error(message: "Missing params")
   end
 
   def update
@@ -13,5 +17,26 @@ class Api::V1::Me::ProjectsController < Api::BaseController
 
   def destroy
 
+  end
+
+  private
+
+  def projects_params
+      params.require(:project).permit(
+        :title,
+        :youtube_id,
+        :room_max,
+        :in_competition,
+        :is_released,
+        :cover_id
+      )
+    rescue
+      nil
+  end
+
+  private
+
+  def render_error(status: 404, message: "Not found")
+    render json: { status: status, errors: { message: message } }, status: status
   end
 end
