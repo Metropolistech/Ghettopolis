5f5b3bfc02611e42b333f139d807e1e9cc1537ba
alexisHetic <alex.delvaque@gmail.com>
2016-01-21 14:50:16 +0100

    Refactor create_user to DRy it

--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -1,8 +1,12 @@
 # This is a helper file to render the test DRY
 
 # Create an user
-def create_user(save: true, data: { username: "DonutLover", email: "homer@contact.com", password: "KillBart", password_confirmation: "KillBart" })
+def create_user(save: true, data: { username: "DonutLover", email: "homer@contact.com" })
   user = User.new(data)
+
+  user.password = data[:password].blank? ? "KillBart" :  data[:password]
+  user.password_confirmation = data[:password].blank? ? user.password : data[:password]
+
   user.save! if save
   block_given? ? (yield user) : user
 end
