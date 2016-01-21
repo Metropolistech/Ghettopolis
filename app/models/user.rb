e1f2164b6ddd033907bdb66d4a18985f73cfe227
alexisHetic <alex.delvaque@gmail.com>
2016-01-21 14:51:52 +0100

    Add User create,update,delete a Project

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,31 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :projects, foreign_key: "author_id", class_name: "Project"
+
   belongs_to :avatar, :class_name => 'Image', :foreign_key => 'image_id'
 
   validates :username, :email, presence: true, uniqueness: true
-  
+
   validates_format_of :email, with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  def create_project!(data: {})
+    project = Project.new(data.merge(author_id: self.id))
+    project.save ? project : false
+  end
+
+  def update_project!(project_id: nil, data: {})
+      project = self.projects.find(project_id)
+      project.attributes = data
+      project.save ? project : false
+    rescue
+      false
+  end
+
+  def delete_project!(project_id)
+      project = self.projects.find(project_id)
+      project.destroy
+    rescue
+      false
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -88,4 +88,170 @@ RSpec.describe User, type: :model do
       end
     end
   end
+
+  describe "#create_project" do
+    context "when title is setted" do
+      it "return the project object" do
+        user = create_user
+        data = { title: "new project" }
+        expect(user.create_project!(data: data)).to be_a Project
+        expect(user.projects.count).to eq(1)
+      end
+    end
+
+    context "when title is not setted" do
+      it "return false to say that project is not created" do
+        user = create_user
+        expect(user.create_project!).to eq(false)
+        expect(user.projects.count).to eq(0)
+      end
+    end
+
+    context "when title is already taken" do
+      it "return false to say that project is not created" do
+        user_a = create_user
+
+        user_b = create_user(data: {
+          username: "MargeSimps",
+          email: "marge@contact.com"
+        })
+
+        user_a.create_project!(data: { title: "hello" })
+
+        expect(user_b.create_project!(data: { title: "hello" })).to eq(false)
+        expect(user_b.projects.count).to eq(0)
+      end
+    end
+  end
+
+  describe "#update_project" do
+    context "when user want update his project" do
+      user = nil
+      project = nil
+
+      before do
+        user = create_user
+        project = user.create_project!(data: { title: "new project" })
+      end
+
+      it "return the updated project" do
+        expect(project.title).to eq("new project")
+
+        updated_project = user.update_project!(project_id: project.id, data: { title: "another title" })
+
+        expect(updated_project.title).to eq("another title")
+        expect(updated_project.id).to eq(project.id)
+      end
+    end
+
+    context "when user want update his project" do
+      user = nil
+      project = nil
+
+      before do
+        user = create_user
+        user_b = create_user(data: { username: "MargeSimps", email: "marge@contact.com" })
+        project = user_b.create_project!(data: { title: "new project" })
+      end
+
+      it "return false to say that the project were not updated" do
+        updated_project = user.update_project!(project_id: project.id, data: { title: "another title" })
+        expect(updated_project).to eq(false)
+      end
+    end
+  end
+
+  describe "#delete_project" do
+    it "can delete a project" do
+      user = create_user
+      project = user.create_project!(data: { title: "new project" })
+      expect(user.projects.count).to eq(1)
+
+      user.delete_project!(project.id)
+      expect(user.projects.count).to eq(0)
+    end
+
+    it "cannot delete a project from other users" do
+      user_a = create_user
+      user_b = create_user(data: { username: "MargeSimps", email: "marge@contact.com" })
+
+      project_b = user_b.create_project!(data: { title: "hello" })
+
+      expect(user_a.delete_project!(project_b.id)).to eq(false)
+    end
+  end
+
+  describe "project methods" do
+    #
+    # it "can put a project in competition" do
+    #   user = create_user
+    #   project_a = user.create_project!(data: { title: "new project" })
+    #   project_b = user.create_project!(data: { title: "second project" })
+    #   user.in_competition_mode!(project_b.id)
+    #   expect(user.projects).to eq(2)
+    #   expect(user.in_competition_projets).to eq(1)
+    # end
+
+    it "cannot remove a project from competition" do
+      pending("Awaiting instructions to implement this test")
+    end
+
+    it "can delete an in_competition project" do
+      pending("Awaiting instructions to implement this test")
+    end
+
+    it "can get projects in_competition" do
+      pending("Awaiting instructions to implement this test")
+    end
+
+    it "can get projects not in_competition" do
+      pending("Awaiting instructions to implement this test")
+    end
+
+    it "can get all projects" do
+      pending("Awaiting instructions to implement this test")
+    end
+
+    # describe "#follow_project & #unfollow_project" do
+    #   user_a = create_user
+    #   user_b = create_user(data: {
+    #     username: "MargeSimps",
+    #     email: "marge@contact.com"
+    #   })
+    #
+    #   project = user_b.create_project!(data: { title: "Marge's project" })
+    #
+    #   it "can follow a project" do
+    #     user_a.follow_project!(project.id)
+    #     expect(user_a.followed_projects.count).to eq(1)
+    #   end
+    #
+    #   it "can unfollow a project" do
+    #     user_a.unfollow_project!(project.id)
+    #     expect(user_a.followed_projects.count).to eq(0)
+    #   end
+    # end
+  end
+
+  describe "self methods" do
+    it "can get his personnal data" do
+      pending("Awaiting instructions to implement this test")
+    end
+
+    it "can update his data" do
+      pending("Awaiting instructions to implement this test")
+    end
+
+    it "can get his followers" do
+      pending("Awaiting instructions to implement this test")
+    end
+
+    it "can get his followings" do
+      pending("Awaiting instructions to implement this test")
+    end
+
+    it "can get his followings projects" do
+      pending("Awaiting instructions to implement this test")
+    end
+  end
 end
