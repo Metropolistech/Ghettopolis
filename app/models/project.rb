368be1a809a68d804d1769540956b57ba5dc87d9
alexisHetic <alex.delvaque@gmail.com>
2016-01-21 14:49:14 +0100

    Add validations to Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,4 @@
 class Project < ActiveRecord::Base
-  belongs_to :author, :class_name => 'User', :foreign_key => 'user_id'
-  belongs_to :cover, :class_name => 'Image', :foreign_key => 'image_id'
+  validates :title, presence: true, uniqueness: true
+  validates :author_id, presence: true
 end
