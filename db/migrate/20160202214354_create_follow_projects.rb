34499f6a445a19b7a81ddefe20002461646e1bac
ADRDra <alexisHetic>
2016-02-02 23:00:18 +0100

    Add references to FollowProject

--- db/migrate/20160202214354_create_follow_projects.rb
+++ db/migrate/20160202214354_create_follow_projects.rb
@@ -3,7 +3,7 @@ class CreateFollowProjects < ActiveRecord::Migration
     create_table :follow_projects do |t|
       t.references :user, index: true, foreign_key: true
       t.references :project, index: true, foreign_key: true
-      
+
       t.timestamps null: false
     end
   end
