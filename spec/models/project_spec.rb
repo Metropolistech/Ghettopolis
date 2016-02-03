6e9f13e4b6b23a45305a24402f1bd5727f18c3c8
ADRDra <alexisHetic>
2016-02-03 14:11:08 +0100

    Add #followers spec

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Project, type: :model do
   describe "validation" do
-    
+
     describe "require a unique title" do
 
     end
@@ -13,6 +13,21 @@ RSpec.describe Project, type: :model do
 
     describe "require a status include in [draft, competition, production, released]" do
 
+    end
+  end
+
+  describe "#followers" do
+    user_a = user_b = project = nil
+    before(:each) do
+      user_a = create_user
+      user_b = create_user data: { username: "MargeSimps", email: "marge@contact.com" }
+      project = user_a.create_project! data: { title: "Marge's project", youtube_id: "1" }
+    end
+
+    it "can get the followers" do
+      expect(project.followers.count).to eq(0)
+      user_b.follow_project! project.id
+      expect(project.followers.count).to eq(1)
     end
   end
 end
