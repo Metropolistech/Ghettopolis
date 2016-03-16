7123ad955498fbbdb1ab98ad8d23cbc5821e08f6
ADRDra <alexisHetic>
2016-03-16 17:51:16 +0100

    Update users route with username param

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   skip_before_filter :authenticate_user_from_token!, only: [:index, :show]
   skip_before_filter :verify_user_confirmation!
-  
+
   # GET /api/v1/users
   def index
     res_send data: User.all
@@ -9,7 +9,7 @@ class Api::V1::UsersController < ApplicationController
 
   # GET /api/v1/users/:id
   def show
-    @user = User.find_by_id(params[:id])
+    @user = User.find_by_username(params[:username])
 
     attributes = params[:populate].blank? ? [] : params[:populate].split(",")
     return res_send data: @user.populate(attributes) if @user
@@ -20,7 +20,7 @@ class Api::V1::UsersController < ApplicationController
   # PUT /api/v1/users/:id
   def update
     if users_params
-      @user = User.find_by_id(params[:id])
+      @user = User.find_by_username(params[:username])
 
       return res_send status: 204 if @user.blank?
       return res_send status: 401  if @user.id != current_user.id
