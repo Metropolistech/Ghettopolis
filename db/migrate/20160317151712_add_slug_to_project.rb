cb5fda12ccb391c647927e370b9743b3737e2e8a
ADRDra <alexisHetic>
2016-03-17 17:55:12 +0100

    Refacto add_slug migration

--- db/migrate/20160317151712_add_slug_to_project.rb
+++ db/migrate/20160317151712_add_slug_to_project.rb
@@ -1,6 +1,6 @@
 class AddSlugToProject < ActiveRecord::Migration
   def up
-    add_column :projects, :slug, :string, unique: true, index: true
+    add_column :projects, :slug, :string, unique: true, index: true unless column_exists? :projects, :slug
   end
 
   def down
