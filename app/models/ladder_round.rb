c27a6d0e80f0a74e67622a51b056814a7823517c
ADRDra <alexisHetic>
2016-03-23 14:47:57 +0100

    Add validates inclusion to LadderRound

--- app/models/ladder_round.rb
+++ app/models/ladder_round.rb
@@ -1,4 +1,8 @@
 class LadderRound < ActiveRecord::Base
   belongs_to :winner, :class_name => 'User', :foreign_key => 'winner_id'
   belongs_to :last_updater, :class_name => 'User', :foreign_key => 'last_updater_id'
+
+  validates :status, inclusion: {
+    in: ["running", "finished"]
+  }
 end
