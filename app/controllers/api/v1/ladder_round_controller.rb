c46375e6f3fe6f114072ab7af9d687acde92c9f7
ADRDra <alexisHetic>
2016-03-24 18:41:41 +0100

    Create LadderRoundController to update current round

---
+++ app/controllers/api/v1/ladder_round_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::LadderRoundController < ApplicationController
+  before_filter :required_params, only: [:update]
+
+  def update
+    res_send status: 401 unless current_user.is_admin
+    @round = LadderRound.current_round
+    @round.update({ date: ladder_round_params, last_updater: current_user })
+    res_send data: @round
+  end
+
+  private
+    def ladder_round_params
+      params.require(:date)
+    end
+
+    def required_params
+      return res_send data:[ActionRecord: "Missing required project parameter"], error: true unless ladder_round_params
+    end
+end
