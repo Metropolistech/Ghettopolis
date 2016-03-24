f54b06fac1b47e7a49718bd478bfe5976899f09a
ADRDra <alexisHetic>
2016-03-24 16:33:55 +0100

    Update winner class_name to Project in LadderRound

--- app/models/ladder_round.rb
+++ app/models/ladder_round.rb
@@ -1,5 +1,5 @@
 class LadderRound < ActiveRecord::Base
-  belongs_to :winner, :class_name => 'User', :foreign_key => 'winner_id'
+  belongs_to :winner, :class_name => 'Project', :foreign_key => 'winner_id'
   belongs_to :last_updater, :class_name => 'User', :foreign_key => 'last_updater_id'
 
   validates :status, inclusion: {
