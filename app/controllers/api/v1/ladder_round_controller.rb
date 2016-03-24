5cd8795b1e3bd4a4d4973541c47f00da539b4b1e
ADRDra <alexisHetic>
2016-03-24 19:19:46 +0100

    Fix LadderRoundController

--- app/controllers/api/v1/ladder_round_controller.rb
+++ app/controllers/api/v1/ladder_round_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::LadderRoundController < ApplicationController
   before_filter :required_params, only: [:update]
 
+  # PUT /api/v1/round
   def update
-    res_send status: 401 unless current_user.is_admin
+    return res_send status: 401 unless current_user.is_admin
     @round = LadderRound.current_round
     @round.update({ date: ladder_round_params, last_updater: current_user })
     res_send data: @round
@@ -10,7 +11,9 @@ class Api::V1::LadderRoundController < ApplicationController
 
   private
     def ladder_round_params
-      params.require(:date)
+        params.require(:date)
+      rescue
+        nil
     end
 
     def required_params
