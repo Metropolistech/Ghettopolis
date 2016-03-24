e189079b57a059ac9cb99ba7789e1ebca451b45e
ADRDra <alexisHetic>
2016-03-24 18:21:37 +0100

    Add email sending

--- app/workers/ladder_round_worker.rb
+++ app/workers/ladder_round_worker.rb
@@ -3,7 +3,19 @@ class LadderRoundWorker
   attr_accessor :round
 
   def run_job
-
+    self
+      .get_current_running_round
+      .get_winner_project
+      .update_winner_project_status
+      .get_ladder_state
+      .send_mail_to_winner_project_author
+      .close_current_round
+      .open_new_round
+    rescue Exception => e
+      ReportMailer
+        .send_worker_fail_report(message: e.message)
+        .deliver_now
+      print "Stop LadderRoundWorker : #{e.message}"
   end
 
   def get_current_running_round
@@ -27,10 +39,12 @@ class LadderRoundWorker
 
   def get_ladder_state
     @round.update({ ladder_state: @ladder.to_json })
+    self
   end
 
   def update_winner_project_status
     @round.winner.update({ status: "production" })
+    self
   end
 
   def round_is_finished?
@@ -39,26 +53,22 @@ class LadderRoundWorker
   end
 
   def send_mail_to_winner_project_author
-
+    CongratsWinnerMailer
+      .send_congrats(to: @round.winner)
+      .deliver_later
+    self
   end
 
   def close_current_round
-
+    @round.update({ status: "finished" })
+    self
   end
 
   def open_new_round
-
+    LadderRound.create
   end
 
   def raise_error(message: nil)
   raise StandardError, "Stop LadderRoundWorker : #{message}"
   end
-
-# GET winner,
-# SET winner.status to: :in_production
-# SEND Mail to winner.author
-# GET Ladder data
-# EXPORT Model.entry to: CSV
-# SEND CSV to: admins
-# CREATE new LadderRound
 end
