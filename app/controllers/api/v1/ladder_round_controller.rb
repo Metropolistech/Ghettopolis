95cc417b99b9cbd9adc0fcacee1009897b11c928
ADRDra <alexisHetic>
2016-03-30 15:44:18 +0200

    Accept Timestamp

--- app/controllers/api/v1/ladder_round_controller.rb
+++ app/controllers/api/v1/ladder_round_controller.rb
@@ -5,7 +5,8 @@ class Api::V1::LadderRoundController < ApplicationController
   def update
     return res_send status: 401 unless current_user.is_admin
     @round = LadderRound.current_round
-    @round.update({ date: ladder_round_params, last_updater: current_user })
+    time = timestamp_to_date
+    @round.update({ date: time, last_updater: current_user })
     res_send data: @round
   end
 
@@ -17,6 +18,10 @@ class Api::V1::LadderRoundController < ApplicationController
     end
 
     def required_params
-      return res_send data:[ActionRecord: "Missing required project parameter"], error: true unless ladder_round_params
+      return res_send data:[ActionRecord: "Missing required date parameter"], error: true unless ladder_round_params
+    end
+
+    def timestamp_to_date
+      Time.at(ladder_round_params)
     end
 end
