898528fcb914bc5ef15a8df07fa12dee0d7c3519
ADRDra <alexisHetic>
2016-04-06 17:15:22 +0200

    add released_youtube_id to Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -13,7 +13,7 @@ class Project < ActiveRecord::Base
 
   belongs_to :author, :class_name => 'User', :foreign_key => 'author_id'
 
-  validates :youtube_id, uniqueness: true
+  validates :youtube_id, :released_youtube_id, uniqueness: true
   validates :youtube_id, :title, :author_id, :description, presence: true
   validates :status, inclusion: { in: ["draft", "competition", "production", "released"] }
 
