3f4d241339edfec6617c1cd3bc39afe7f6daf27d
ADRDra <alexisHetic>
2016-05-30 11:46:54 +0200

    Add Tags in search

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -6,12 +6,18 @@ class Api::V1::SearchController < ApplicationController
     @q = params[:query]
 
     res_send data: {
+      tags: search_tags,
       users: search_users,
       projects: search_projects
     }
   end
 
   private
+    def search_tags
+      ActsAsTaggableOn::Tag
+        .where("LOWER(name) LIKE :query", query: "%#{@q.downcase}%")
+    end
+
     def search_users
       User.search(@q)
     end
