2adea5381495d17e6ca8d8997e5dbd636906ca5e
ADRDra <alexisHetic>
2016-05-30 11:52:40 +0200

    Add phone to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160530094942) do
+ActiveRecord::Schema.define(version: 20160530095135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -167,6 +167,7 @@ ActiveRecord::Schema.define(version: 20160530094942) do
     t.string   "firstname"
     t.text     "bio"
     t.jsonb    "networks",               default: {"twitter"=>nil, "youtube"=>nil, "facebook"=>nil}, null: false
+    t.string   "phone"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
