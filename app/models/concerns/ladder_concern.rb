7c598764a3c72a31c7bd47134fc186dd1e74cf77
ADRDra <alexisHetic>
2016-05-31 10:49:20 +0200

    Update ladder algorithm

--- app/models/concerns/ladder_concern.rb
+++ app/models/concerns/ladder_concern.rb
@@ -7,33 +7,51 @@ module LadderConcern
         .available
         .in_competion
         .map { |project|
-          project
-          .ladder_score =
-            score_by_date(project) +
-            score_by_comments(project) +
-            score_by_votes(project) +
-            score_by_author_created_at(project)
-          project
+          @project = project
+          @project.ladder_score = 1000 + calc_score
+          @project
         }
         .sort_by { |project| project.ladder_score }
         .reverse
     end
 
+    # An util to get the difference in days between today and a specific date
+    def get_date_difference(date)
+      (Date.today - date).to_i
+    end
+
     private
-      def score_by_date(project)
-        - (Date.today - project.competition_at.to_date).to_i * 10
+      def calc_score
+        score_by_days +
+        score_by_weeks +
+        score_by_comments +
+        score_by_votes +
+        score_by_author_created_at +
+        score_by_author_projects
       end
 
-      def score_by_comments(project)
-        project.comments.keys.count * 5
+      def score_by_days
+        - get_date_difference(@project.competition_at.to_date) * 4
       end
 
-      def score_by_votes(project)
-        project.followers_count * 15
+      def score_by_weeks
+        - (get_date_difference(@project.competition_at.to_date) / 7) * 20
       end
 
-      def score_by_author_created_at(project)
-        (Date.today - project.author.created_at.to_date).to_i * 2
+      def score_by_comments
+        @project.comments.keys.count * 5
+      end
+
+      def score_by_votes
+        @project.followers_count * 40
+      end
+
+      def score_by_author_created_at
+        (get_date_difference(@project.author.created_at.to_date) / 30) * 2
+      end
+
+      def score_by_author_projects
+        @project.author.projects.count * 2
       end
   end
 end
