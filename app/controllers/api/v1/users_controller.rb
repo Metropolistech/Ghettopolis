e44cd9863c2f3db32b4ec736b6f403b86635491a
ADRDra <alexisHetic>
2016-06-02 11:45:40 +0200

    User admin can update other profiles

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -25,16 +25,16 @@ class Api::V1::UsersController < ApplicationController
   # GET /api/v1/users/:id
   def show
     attributes = params[:populate].blank? ? [] : params[:populate].split(",")
-    return res_send data: @user.populate(attributes) if @user
+    return res_send data: @requested_user.populate(attributes) if @requested_user
     res_send(status: 204)
   end
 
   # PUT /api/v1/users/:id
   def update
-    return res_send status: 204 if @user.blank?
-    return res_send status: 401 if @user.id != current_user.id
-    return res_send data: @user if @user.update(@filtered_params)
-    res_send data: @user.errors.messages, error: true
+    return res_send status: 204 if @requested_user.blank?
+    return res_send status: 401 if @requested_user.id != current_user.id && !current_user.is_admin
+    return res_send data: @requested_user if @requested_user.update(@filtered_params)
+    res_send data: @requested_user.errors.messages, error: true
   end
 
   def destroy
@@ -67,7 +67,7 @@ class Api::V1::UsersController < ApplicationController
 
     def find_user_by_username_or_id
       _id = params[:id]
-      @user = User.find_by_lower_username(_id).first || User.find_by_id(_id)
+      @requested_user = User.find_by_lower_username(_id).first || User.find_by_id(_id)
     end
 
     def filtered_params
